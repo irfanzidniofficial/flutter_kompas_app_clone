f66eb6c1053acd5bd5cca194da2d389b3bf09bf0
frdnhmrf <frdnhmrf@gmail.com>
2023-02-10 09:05:37 +0700

    merge changes

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_kompas_app_clone/src/features/account/edit_profile_screen.dart';
 import 'package:flutter_kompas_app_clone/src/features/account/profile_screen.dart';
-import 'package:flutter_kompas_app_clone/src/features/account/setting_screen.dart';
-import 'package:flutter_kompas_app_clone/src/features/authentication/presentation/onboarding/onboarding_screen.dart';
+import 'package:flutter_kompas_app_clone/src/features/authentication/presentation/onboarding_screen.dart';
 import 'package:flutter_kompas_app_clone/src/features/authentication/presentation/sign_in_screen.dart';
 import 'package:flutter_kompas_app_clone/src/features/authentication/presentation/sign_up_scren.dart';
+<<<<<<< HEAD
 
 import 'package:flutter_kompas_app_clone/src/features/menu/presentasion/detail_menu_screen.dart';
 
@@ -13,6 +13,8 @@ import 'package:flutter_kompas_app_clone/src/features/search/presentation/search
 import 'package:flutter_kompas_app_clone/src/routing/main_screen.dart';
 
 import 'package:flutter_kompas_app_clone/src/features/search/search_screen.dart';
+=======
+>>>>>>> 5e9f960 (merge changes)
 import 'package:go_router/go_router.dart';
 
 import '../features/menu/presentasion/menu_screen.dart';
@@ -22,6 +24,7 @@ final goRouter = GoRouter(
   debugLogDiagnostics: true,
   routes: [
     GoRoute(
+<<<<<<< HEAD
       path: '/',
       builder: (context, state) => const OnboardingScreen(),
       routes: [
@@ -71,5 +74,27 @@ final goRouter = GoRouter(
         ),
       ],
     ),
+=======
+        path: '/',
+        builder: (context, state) => const OnboardingScreen(),
+        routes: [
+          GoRoute(
+            path: 'sign-in',
+            builder: (context, state) => const SignInScreen(),
+          ),
+          GoRoute(
+            path: 'sign-up',
+            builder: (context, state) => const SignUpScreen(),
+          ),
+          GoRoute(
+            path: 'profile',
+            builder: (context, state) => const ProfileScreen(),
+          ),
+          GoRoute(
+            path: 'edit-profile',
+            builder: (context, state) => const EditProfileScreen(),
+          ),
+        ]),
+>>>>>>> 5e9f960 (merge changes)
   ],
 );
